e4c95b03fc35db8d1181289a7c55a5b08c381373
QuentinVGS <qveigas@gmail.com>
2025-09-24 12:36:49 +0200

    Etape 3 : ajout de la fonction pour afficher la grille

--- puissance4.py
+++ puissance4.py
@@ -3,3 +3,8 @@ COLS = 7
 
 def create_grid():
     return [[" " for _ in range(COLS)] for _ in range(ROWS)]
+
+def print_grid(grid):
+    for row in grid:
+        print("| " + " | ".join(row) + " |")
+    print("  " + "   ".join(str(i) for i in range(COLS)))
